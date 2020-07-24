b139b80965cd5d9f3b51af01935ae682a20a89f7
Darshan Kathiriya <darshankathiriya121@gmail.com>
2020-07-24 00:52:27 -0300

    added basic add and mul operator

--- src/ops.rs
+++ src/ops.rs
@@ -1,16 +1,25 @@
 use crate::variable::*;
 use std::cell::RefCell;
 use std::rc::Rc;
+
+pub trait Function {
+    fn apply(&self, a: &Rc<RefCell<Variable>>, b: &Rc<RefCell<Variable>>) -> Variable{
+        Self::forward(a,b)
+    }
+    fn forward(a: &Rc<RefCell<Variable>>, b: &Rc<RefCell<Variable>>) -> Variable;
+}
 pub trait GradFunction {
     fn apply(&self, grad: &Variable);
 }
 
-pub struct Add;
 
-impl Add {
-    pub fn apply(&self, a: &Rc<RefCell<Variable>>, b: &Rc<RefCell<Variable>>) -> Variable {
+pub struct Add;
+pub struct Mul;
+
+impl Function for Add {
+    fn forward(a: &Rc<RefCell<Variable>>, b: &Rc<RefCell<Variable>>) -> Variable {
         let data = a.borrow().data + b.borrow().data;
-        let grad_fn = AddBackWard {
+        let grad_fn = AddBackward {
             x: Rc::clone(a),
             y: Rc::clone(b),
         };
@@ -22,12 +31,46 @@ impl Add {
     }
 }
 
-pub struct AddBackWard {
+impl Function for Mul {
+    fn forward(a: &Rc<RefCell<Variable>>, b: &Rc<RefCell<Variable>>) -> Variable {
+        let data = a.borrow().data * b.borrow().data;
+        let grad_fn = MulBackward {
+            x: Rc::clone(a),
+            y: Rc::clone(b),
+        };
+        Variable {
+            data,
+            grad: None,
+            grad_fn: Some(Rc::new(RefCell::new(grad_fn))),
+        }
+    }
+}
+
+pub struct AddBackward {
     x: Rc<RefCell<Variable>>,
     y: Rc<RefCell<Variable>>,
 }
 
-impl GradFunction for AddBackWard {
+impl GradFunction for AddBackward {
+    fn apply(&self, grad: &Variable) {
+        self.x.borrow_mut().backward(Some(&Variable {
+            data: grad.data,
+            grad: None,
+            grad_fn: None,
+        }));
+        self.y.borrow_mut().backward(Some(&Variable {
+            data: grad.data,
+            grad: None,
+            grad_fn: None,
+        }));
+    }
+}
+pub struct MulBackward {
+    x: Rc<RefCell<Variable>>,
+    y: Rc<RefCell<Variable>>,
+}
+
+impl GradFunction for MulBackward {
     fn apply(&self, grad: &Variable) {
         self.x.borrow_mut().backward(Some(&Variable {
             data: grad.data * self.y.borrow().data,
@@ -78,6 +121,40 @@ mod tests {
             grad_fn: None,
         }));
         let op = Add {};
+        let mut out = op.apply(&a, &b);
+        out.backward(None);
+        assert_eq!(1.0, a.borrow().grad.unwrap());
+        assert_eq!(1.0, b.borrow().grad.unwrap());
+    }
+    #[test]
+    fn test_mul_op() {
+        let a = Rc::new(RefCell::new(Variable {
+            data: 2.0,
+            grad: None,
+            grad_fn: None,
+        }));
+        let b = Rc::new(RefCell::new(Variable {
+            data: 3.0,
+            grad: None,
+            grad_fn: None,
+        }));
+        let op = Mul {};
+        let out = op.apply(&a, &b);
+        assert_eq!(6.0, out.data);
+    }
+    #[test]
+    fn test_mul_backward() {
+        let a = Rc::new(RefCell::new(Variable {
+            data: 2.0,
+            grad: None,
+            grad_fn: None,
+        }));
+        let b = Rc::new(RefCell::new(Variable {
+            data: 3.0,
+            grad: None,
+            grad_fn: None,
+        }));
+        let op = Mul {};
         let mut out = op.apply(&a, &b);
         out.backward(None);
         assert_eq!(3.0, a.borrow().grad.unwrap());
