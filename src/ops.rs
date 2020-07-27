77ebbcdd06b749d04a08bbfd9529bb7be5ac8a35
Darshan Kathiriya <darshankathiriya121@gmail.com>
2020-07-27 19:09:50 -0300

    Works with Rc<RefCell<VariableImpl<T>>>

--- src/ops.rs
+++ src/ops.rs
@@ -7,92 +7,89 @@ use std::rc::Rc;
 // use crate::binary_ops;
 
 pub trait Function<T> {
-    fn apply(&self, a: &Variable<T>, b: &Variable<T>) -> Variable<T> {
+    fn apply(&self, a: &Rc<RefCell<VariableImpl<T>>>, b: &Rc<RefCell<VariableImpl<T>>>) -> VariableImpl<T> {
         Self::forward(a, b)
     }
-    fn forward(a: &Variable<T>, b: &Variable<T>) -> Variable<T>;
+    fn forward(a: &Rc<RefCell<VariableImpl<T>>>, b: &Rc<RefCell<VariableImpl<T>>>) -> VariableImpl<T>;
 }
 pub trait GradFunction<T> {
-    fn apply(&self, grad: &NdArray<T>);
+    fn apply(&mut self, grad: &NdArray<T>);
 }
 
 pub struct Add;
 pub struct Mul;
 
 impl<T: Float> Function<T> for Add {
-    fn forward(a: &Variable<T>, b: &Variable<T>) -> Variable<T> {
-        let a_borrowed = a.data.as_ref().as_ptr();
-        let b_borrowed = b.data.as_ref().as_ptr();
+    fn forward(a: &Rc<RefCell<VariableImpl<T>>>, b: &Rc<RefCell<VariableImpl<T>>>) -> VariableImpl<T> {
+        let a_borrowed = &a.borrow().data;
+        let b_borrowed = &b.borrow().data;
 
-        let data = unsafe { a_borrowed.as_ref().unwrap() + b_borrowed.as_ref().unwrap() };
-        // let grad_fn = AddBackward {
-        //     x: Rc::clone(a),
-        //     y: Rc::clone(b),
-        // };
-        Variable {
-            data: Rc::new(RefCell::new(data)),
+        let data = a_borrowed + b_borrowed;
+        let grad_fn = AddBackward::new(a.clone(), b.clone());
+        VariableImpl {
+            data: data,
             grad: None,
-            grad_fn: None,
+            grad_fn: Some(Rc::new(RefCell::new(grad_fn))),
         }
     }
 }
 
-// impl<T> Function<T> for Mul {
-//     fn forward(a: &Rc<RefCell<Variable<T>>>, b: &Rc<RefCell<Variable<T>>>) -> Variable<T> {
-//         let a_borrowed = unsafe{(*a.as_ptr()).data.get_mut()};
-//         let b_borrowed = unsafe{(*b.as_ptr()).data.get_mut()};
+impl<T: Float> Function<T> for Mul {
+    fn forward(a: &Rc<RefCell<VariableImpl<T>>>, b: &Rc<RefCell<VariableImpl<T>>>) -> VariableImpl<T> {
+        let a_borrowed = &a.borrow().data;
+        let b_borrowed = &b.borrow().data;
 
-//         let data = a_borrowed * b_borrowed;
-//         let grad_fn = MulBackward {
-//             x: Rc::clone(a),
-//             y: Rc::clone(b),
-//         };
-//         Variable {
-//             data,
-//             grad: None,
-//             grad_fn: Some(Rc::new(RefCell::new(grad_fn))),
-//         }
-//     }
-// }
+        let data = a_borrowed * b_borrowed;
+        let grad_fn = MulBackward {
+            x: a.clone(),
+            y: b.clone(),
+        };
+        VariableImpl {
+            data,
+            grad: None,
+            grad_fn: Some(Rc::new(RefCell::new(grad_fn))),
+        }
+    }
+}
 
-// pub struct AddBackward<T> {
-//     x: Rc<RefCell<Variable<T>>>,
-//     y: Rc<RefCell<Variable<T>>>,
-// }
+pub struct AddBackward<T> {
+    x: Rc<RefCell<VariableImpl<T>>>,
+    y: Rc<RefCell<VariableImpl<T>>>,
+}
 
-// impl<T> GradFunction<T> for AddBackward<T> {
-//     fn apply(&self, grad: &Variable<T>) {
-//         self.x.borrow_mut().backward(Some(&Variable {
-//             data: grad.data,
-//             grad: None,
-//             grad_fn: None,
-//         }));
-//         self.y.borrow_mut().backward(Some(&Variable {
-//             data: grad.data,
-//             grad: None,
-//             grad_fn: None,
-//         }));
-//     }
-// }
-// pub struct MulBackward<T> {
-//     x: Rc<RefCell<Variable<T>>>,
-//     y: Rc<RefCell<Variable<T>>>,
-// }
+impl<T> AddBackward<T> {
+    pub fn new(x: Rc<RefCell<VariableImpl<T>>>, y: Rc<RefCell<VariableImpl<T>>>) -> Self {
+        AddBackward { x, y }
+    }
+}
 
-// impl<T> GradFunction<T> for MulBackward<T> {
-//     fn apply(&self, grad: &Variable<T>) {
-//         self.x.borrow_mut().backward(Some(&Variable {
-//             data: grad.data * self.y.borrow().data,
-//             grad: None,
-//             grad_fn: None,
-//         }));
-//         self.y.borrow_mut().backward(Some(&Variable {
-//             data: grad.data * self.x.borrow().data,
-//             grad: None,
-//             grad_fn: None,
-//         }));
-//     }
-// }
+impl<T> GradFunction<T> for AddBackward<T>
+where
+    T: Float,
+{
+    fn apply(&mut self, grad: &NdArray<T>) {
+        self.x.borrow_mut().backward(Some(grad));
+        self.y.borrow_mut().backward(Some(grad));
+    }
+}
+pub struct MulBackward<T> {
+    x: Rc<RefCell<VariableImpl<T>>>,
+    y: Rc<RefCell<VariableImpl<T>>>,
+}
+
+impl<T> GradFunction<T> for MulBackward<T>
+where
+    T: Float,
+{
+    fn apply(&mut self, grad: &NdArray<T>) {
+        self.x
+            .borrow_mut()
+            .backward(Some(&(grad * &(self.y.borrow().data))));
+        self.y
+            .borrow_mut()
+            .backward(Some(&(grad * &(self.x.borrow().data))));
+    }
+}
 
 // impl<T> fmt::Debug for Variable<T> {
 //     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -152,83 +149,106 @@ impl<T: Float> Function<T> for Add {
 #[cfg(test)]
 mod tests {
     use crate::ops::*;
-    use crate::variable::*;
+    use crate::variable::VariableImpl;
     use std::cell::RefCell;
     use std::rc::Rc;
 
     #[test]
     fn test_add_op() {
-        let a = Variable::<f64>::ones(&[2, 2]);
-        let b = Variable::<f64>::ones(&[2, 2]);
+        let a = Rc::new(RefCell::new(VariableImpl::<f64>::ones(&[2, 2])));
+        let b = Rc::new(RefCell::new(VariableImpl::<f64>::ones(&[2, 2])));
         let op = Add {};
         let out = op.apply(&a, &b);
-        assert_eq!(&[2.0; 4], out.data.borrow().as_slice().unwrap());
+        assert_eq!(&[2.0; 4], out.data.as_slice().unwrap());
         // println!("{:?}", out.data.borrow().as_slice().unwrap())
     }
-//     #[test]
-//     fn test_add_backward() {
-//         let a = Rc::new(RefCell::new(Variable {
-//             data: 2.0,
-//             grad: None,
-//             grad_fn: None,
-//         }));
-//         let b = Rc::new(RefCell::new(Variable {
-//             data: 3.0,
-//             grad: None,
-//             grad_fn: None,
-//         }));
-//         let op = Add {};
-//         let mut out = op.apply(&a, &b);
-//         out.backward(None);
-//         assert_eq!(1.0, a.borrow().grad.unwrap());
-//         assert_eq!(1.0, b.borrow().grad.unwrap());
-//     }
-//     #[test]
-//     fn test_add_overloading(){
-//         let a = Variable{data: 2.0, grad: None, grad_fn: None};
-//         let b = Variable{data: 3.0, grad: None, grad_fn: None};
-//         let c: Variable = a+b;
-//         assert_eq!(5.0, c.data);
-//     }
-//     #[test]
-//     fn test_mul_op() {
-//         let a = Rc::new(RefCell::new(Variable {
-//             data: 2.0,
-//             grad: None,
-//             grad_fn: None,
-//         }));
-//         let b = Rc::new(RefCell::new(Variable {
-//             data: 3.0,
-//             grad: None,
-//             grad_fn: None,
-//         }));
-//         let op = Mul {};
-//         let out = op.apply(&a, &b);
-//         assert_eq!(6.0, out.data);
-//     }
-//     #[test]
-//     fn test_mul_backward() {
-//         let a = Rc::new(RefCell::new(Variable {
-//             data: 2.0,
-//             grad: None,
-//             grad_fn: None,
-//         }));
-//         let b = Rc::new(RefCell::new(Variable {
-//             data: 3.0,
-//             grad: None,
-//             grad_fn: None,
-//         }));
-//         let op = Mul {};
-//         let mut out = op.apply(&a, &b);
-//         out.backward(None);
-//         assert_eq!(3.0, a.borrow().grad.unwrap());
-//         assert_eq!(2.0, b.borrow().grad.unwrap());
-//     }
-//     #[test]
-//     fn test_mul_overloading(){
-//         let a = Variable{data: 2.0, grad: None, grad_fn: None};
-//         let b = Variable{data: 3.0, grad: None, grad_fn: None};
-//         let c: Variable = a*b;
-//         assert_eq!(6.0, c.data);
-//     }
+    #[test]
+    fn test_add_backward() {
+        let mut a = Rc::new(RefCell::new(VariableImpl::from_constant(&[2, 2], 3.0)));
+        let mut b = Rc::new(RefCell::new(VariableImpl::from_constant(&[2, 2], 2.0)));
+        let op = Add {};
+        let mut out = op.apply(&mut a, &mut b);
+        out.backward(None);
+        assert_eq!(
+            &[1.0; 4],
+            a.borrow().grad.as_ref().unwrap().as_slice().unwrap()
+        );
+        assert_eq!(
+            &[1.0; 4],
+            b.borrow().grad.as_ref().unwrap().as_slice().unwrap()
+        );
+    }
+    #[test]
+    fn test_mul_op() {
+        let a = Rc::new(RefCell::new(VariableImpl::<f64>::ones(&[2, 2])));
+        let b = Rc::new(RefCell::new(VariableImpl::<f64>::ones(&[2, 2])));
+        let op = Mul {};
+        let out = op.apply(&a, &b);
+        assert_eq!(&[1.0; 4], out.data.as_slice().unwrap());
+        // println!("{:?}", out.data.borrow().as_slice().unwrap())
+    }
+    #[test]
+    fn test_mul_backward() {
+        let mut a = Rc::new(RefCell::new(VariableImpl::from_constant(&[2, 2], 3.0)));
+        let mut b = Rc::new(RefCell::new(VariableImpl::from_constant(&[2, 2], 2.0)));
+        let op = Mul {};
+        let mut out = op.apply(&mut a, &mut b);
+        out.backward(None);
+        assert_eq!(
+            &[2.0; 4],
+            a.borrow().grad.as_ref().unwrap().as_slice().unwrap()
+        );
+        assert_eq!(
+            &[3.0; 4],
+            b.borrow().grad.as_ref().unwrap().as_slice().unwrap()
+        );
+    }
+    //     #[test]
+    //     fn test_add_overloading(){
+    //         let a = Variable{data: 2.0, grad: None, grad_fn: None};
+    //         let b = Variable{data: 3.0, grad: None, grad_fn: None};
+    //         let c: Variable = a+b;
+    //         assert_eq!(5.0, c.data);
+    //     }
+    //     #[test]
+    //     fn test_mul_op() {
+    //         let a = Rc::new(RefCell::new(Variable {
+    //             data: 2.0,
+    //             grad: None,
+    //             grad_fn: None,
+    //         }));
+    //         let b = Rc::new(RefCell::new(Variable {
+    //             data: 3.0,
+    //             grad: None,
+    //             grad_fn: None,
+    //         }));
+    //         let op = Mul {};
+    //         let out = op.apply(&a, &b);
+    //         assert_eq!(6.0, out.data);
+    //     }
+    //     #[test]
+    //     fn test_mul_backward() {
+    //         let a = Rc::new(RefCell::new(Variable {
+    //             data: 2.0,
+    //             grad: None,
+    //             grad_fn: None,
+    //         }));
+    //         let b = Rc::new(RefCell::new(Variable {
+    //             data: 3.0,
+    //             grad: None,
+    //             grad_fn: None,
+    //         }));
+    //         let op = Mul {};
+    //         let mut out = op.apply(&a, &b);
+    //         out.backward(None);
+    //         assert_eq!(3.0, a.borrow().grad.unwrap());
+    //         assert_eq!(2.0, b.borrow().grad.unwrap());
+    //     }
+    //     #[test]
+    //     fn test_mul_overloading(){
+    //         let a = Variable{data: 2.0, grad: None, grad_fn: None};
+    //         let b = Variable{data: 3.0, grad: None, grad_fn: None};
+    //         let c: Variable = a*b;
+    //         assert_eq!(6.0, c.data);
+    //     }
 }
--- src/variable.rs
+++ src/variable.rs
@@ -1,28 +1,39 @@
 use crate::ndarry_ext::*;
-use crate::ops;
+use crate::ops::*;
 use std::cell::RefCell;
 use std::rc::Rc;
-pub struct Variable<T> {
-    pub data: Rc<RefCell<NdArray<T>>>,
+
+pub struct VariableImpl<T> {
+    pub data: NdArray<T>,
     pub grad: Option<NdArray<T>>,
-    pub grad_fn: Option<Rc<RefCell<dyn ops::GradFunction<T>>>>,
+    pub grad_fn: Option<Rc<RefCell<dyn GradFunction<T>>>>,
 }
-impl<T: Float> Variable<T> {
-    pub fn ones(shape: &[usize]) -> Variable<T> {
-        Variable {
-            data: Rc::new(RefCell::new(NdArray::<T>::ones(shape))),
+
+impl<T: Float> VariableImpl<T> {
+    pub fn ones(shape: &[usize]) -> VariableImpl<T> {
+        VariableImpl {
+            data: NdArray::<T>::ones(shape),
             grad: None,
             grad_fn: None,
         }
     }
+
+    pub fn from_constant(shape: &[usize], constant: T) -> VariableImpl<T> {
+        VariableImpl {
+            data: NdArray::<T>::from_elem(shape, constant),
+            grad: None,
+            grad_fn: None,
+        }
+    }
+
     pub fn backward(&mut self, grad_arg: Option<&NdArray<T>>) {
         if let Some(g) = grad_arg {
             self.grad = Some(g.clone());
         } else {
-            self.grad = Some(NdArray::<T>::ones(self.data.borrow().shape()));
+            self.grad = Some(NdArray::<T>::ones(self.data.shape()));
         }
-        if let Some(grad_fn) = self.grad_fn.as_ref() {
-            grad_fn.borrow().apply(self.grad.as_ref().unwrap());
+        if let Some(grad_fn) = self.grad_fn.clone() {
+            grad_fn.borrow_mut().apply(self.grad.as_ref().unwrap());
         }
     }
 }
